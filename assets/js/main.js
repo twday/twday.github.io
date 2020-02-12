5fbe8f9282026cbdd9246cbe20781eb33f0e5433
Tom Day <t.day@chester.ac.uk>
2020-02-12 15:41:17 +0000

    Added Nav Bar

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,27 @@
 var body = document.body;
 var header = document.querySelector(".header__title");
+var sections = document.querySelectorAll("section");
 
 window.onload = (e) => {
     window.onscroll = (e) => {
-        if (this.scrollY > 400){
+        if (this.scrollY > sections[0].offsetHeight){
             header.classList.remove("header__title--hidden");
         } else {
             header.classList.add("header__title--hidden");
         }
+
+        var height = (window.innerHeight || document.documentElement.clientHeight);
+        sections.forEach((section, index) => {
+            var bounds = section.getBoundingClientRect();
+            var id = section.querySelector('a').id;
+            var link = document.querySelector("a[href='#"+id+"']");
+
+            if (bounds.top >= 0 && bounds.bottom <= height){
+                link.classList.add("nav-item_active");
+            } else {
+                link.classList.remove("nav-item_active");
+            }
+
+        });
     };
 };
